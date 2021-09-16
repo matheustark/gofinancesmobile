5e18cc9d4ea0150e260b8afe7bcb546267e01096
Matheus Gonçalves <matheustark321@gmail.com>
2021-09-15 21:55:03 -0300

    create Graph interface

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Platform } from 'react-native';
+
 import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
-import { Platform } from 'react-native';
+import { Resume } from '../screens/Resume';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -52,7 +54,7 @@ export function AppRoutes() {
             />
             <Screen 
              name="Resumo"
-             component={Register}
+             component={Resume}
              options={{
                 tabBarIcon: (({ size, color }) => (
                     <MaterialIcons 
