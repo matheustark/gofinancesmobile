780cecadf0bb82790f75084abcd543aa0d6e37c2
Matheus Gonçalves <matheustark321@gmail.com>
2021-09-16 00:08:08 -0300

    render graph

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { VictoryPie } from 'victory-native';
+
 import { HistoryCard } from '../../components/HistoryCard';
 
 import { 
@@ -22,7 +24,8 @@ interface TransactionProps {
 interface CategoryData {
     key: string;
     name: string;
-    total: string;
+    total: number;
+    totalFormatted: string;
     color: string;
 }
 
@@ -50,7 +53,7 @@ export function Resume() {
           });
 
           if (categorySum > 0) {
-            const total = categorySum
+            const totalFormatted = categorySum
             .toLocaleString('pt-BR', { 
                 style: 'currency',
                 currency: 'BRL'
@@ -60,7 +63,8 @@ export function Resume() {
                 key: category.key,
                 name: category.name,
                 color: category.color,
-                total,
+                total: categorySum,
+                totalFormatted,
             })
           }
           
@@ -81,13 +85,18 @@ export function Resume() {
                     Resumo por categoria
                 </Title>
             </Header>
+        <VictoryPie 
+            data={totalByCategories}
+            x="name"
+            y="total"
+        />
         <Content>
            {
            totalByCategories.map(item => (
                <HistoryCard
                key={item.key}
                title={item.name}
-               amount={item.total}
+               amount={item.totalFormatted}
                color={item.color}
                 />
            )) 
